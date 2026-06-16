3e20b6cccd2b5adc48488f3ad53a501a390637a6
Mishidog <jpcreativeconcepts@gmail.com>
2026-06-16 09:14:10 -0700

    Allow AI search crawlers in robots

--- app/robots.js
+++ app/robots.js
@@ -2,12 +2,30 @@ import { site } from "@/data/site";
 
 export const dynamic = "force-static";
 
+const aiSearchBots = [
+  "Amazonbot",
+  "Applebot-Extended",
+  "Bytespider",
+  "CCBot",
+  "ClaudeBot",
+  "CloudflareBrowserRenderingCrawler",
+  "Google-Extended",
+  "GPTBot",
+  "meta-externalagent"
+];
+
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/"
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/"
+      },
+      ...aiSearchBots.map((userAgent) => ({
+        userAgent,
+        allow: "/"
+      }))
+    ],
     sitemap: `${site.url}/sitemap.xml`
   };
 }
